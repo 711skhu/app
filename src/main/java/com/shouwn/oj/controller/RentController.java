df5bc0c1993ecbf645c8f63cc524b063361dc479
Kim Ji Eun <ys6445@naver.com>
2019-04-16 13:17:43 +0900

    세부사항 수정3

---
+++ src/main/java/com/shouwn/oj/controller/RentController.java
@@ -0,0 +1,5 @@
+package com.shouwn.oj.controller;
+
+public class RentController {
+    // 빌릴 때 사용하는 Controller
+}
--- src/main/java/com/shouwn/oj/model/entity/user/UserRentState.java
+++ src/main/java/com/shouwn/oj/model/entity/user/UserRentState.java
@@ -1,7 +1,5 @@
 package com.shouwn.oj.model.entity.user;
 
-import java.util.List;
-
 public class UserRentState extends UserRequest{
     //나의 대여현황을 보여주는 리스트
 
