b95907ea3b0dd9c7da9339ebeed3d0f3be4b56ac
songz1 <21pori30@gmail.com>
2019-05-07 14:38:17 +0900

    Security 수정

--- src/main/java/com/shouwn/oj/MobileApplication.java
+++ src/main/java/com/shouwn/oj/MobileApplication.java
@@ -1,9 +1,13 @@
 package com.shouwn.oj;
 
+import com.shouwn.oj.config.StudentJwtProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StudentJwtProperties.class)
 public class MobileApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/shouwn/oj/config/SecurityConfig.java
+++ src/main/java/com/shouwn/oj/config/SecurityConfig.java
@@ -2,10 +2,15 @@ package com.shouwn.oj.config;
 
 import com.shouwn.oj.model.entity.member.Student;
 import com.shouwn.oj.security.JwtProperties;
+import com.shouwn.oj.security.config.EnableJwtSecurity;
 import com.shouwn.oj.security.config.JwtSecurityConfigurerAdapter;
 import com.shouwn.oj.service.member.MemberAuthService;
 import com.shouwn.oj.service.member.StudentService;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableJwtSecurity
 public class SecurityConfig extends JwtSecurityConfigurerAdapter {
 
 	private final StudentService studentService;
