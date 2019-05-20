73dfd952e991b6a3221a8be7d12db4ab4ddf9b76
Kim Ji Eun <ys6445@naver.com>
2019-05-21 02:56:22 +0900

    Revert "Eum 삽입"
    This reverts commit c8a476b02ae80d5dd68c75dcf184315d93a306c4.

--- src/main/java/com/shouwn/oj/controller/rent/RentController.java
+++ src/main/java/com/shouwn/oj/controller/rent/RentController.java
@@ -1,11 +1,5 @@
 package com.shouwn.oj.controller.rent;
 
-import com.shouwn.oj.model.request.user.UserLoginRequest;
-import com.shouwn.oj.model.response.ApiResponse;
-import com.shouwn.oj.model.response.CommonResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,20 +15,4 @@ public class RentController {
 	강의실에 따른 시간별 목록 리스트 -> rent/{lectureCode}/{date}
 	사용자가 강의실 대여 신청 ->rent
 	사용자의 강의실 대여 목록 리스트 ->rent/rentList */
-
-/*	@PostMapping("selectBuilding")
-	public ApiResponse<?> selectBuilding(@RequestBody RentRequest rentRequest) {
-
-		if (LectureRoomService.selectBuilding(rentRequest)) {
-			return CommonResponse.builder()
-					.status(HttpStatus.OK)
-					.message("로그인 성공")
-					.build();
-		} else {
-			return CommonResponse.builder()
-					.status(HttpStatus.FORBIDDEN)
-					.message("로그인 실패. 계정 정보 확인 바랍니다.")
-					.build();
-		}
-	}*/
 }
