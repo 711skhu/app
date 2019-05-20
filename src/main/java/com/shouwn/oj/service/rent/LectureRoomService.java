da8bf6d206190ee4dd173433c5d0135a870201fd
Kim Ji Eun <ys6445@naver.com>
2019-05-21 02:57:27 +0900

    Revert "Revert "Eum 삽입""
    This reverts commit 73dfd952e991b6a3221a8be7d12db4ab4ddf9b76.

--- src/main/java/com/shouwn/oj/service/rent/LectureRoomService.java
+++ src/main/java/com/shouwn/oj/service/rent/LectureRoomService.java
@@ -1,7 +1,22 @@
 package com.shouwn.oj.service.rent;
 
+import java.io.IOException;
+import java.net.URL;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.shouwn.oj.model.request.user.UserLoginRequest;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class LectureRoomService {
+
+	//UserService의 login을 불러오기
+
+	//건물 테이블 보여주기
+
 }
