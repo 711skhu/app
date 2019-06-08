b3fec09c076d706faf99eb20db66d4fd390f3c6c
shouwn <sjonghyon@gmail.com>
2019-06-09 03:04:56 +0900

    core jwt 수정에 따른 jwt 설정 변경 및 서비스 책임 변경에 따른 리팩토링

--- src/main/java/com/shouwn/oj/config/SecurityConfig.java
+++ src/main/java/com/shouwn/oj/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package com.shouwn.oj.config;
 
-import com.shouwn.oj.model.entity.member.Student;
 import com.shouwn.oj.security.JwtProperties;
 import com.shouwn.oj.security.config.EnableJwtSecurity;
 import com.shouwn.oj.security.config.JwtSecurityConfigurerAdapter;
-import com.shouwn.oj.service.member.MemberAuthService;
-import com.shouwn.oj.service.member.StudentService;
 
 import org.springframework.context.annotation.Configuration;
 
@@ -13,18 +10,10 @@ import org.springframework.context.annotation.Configuration;
 @EnableJwtSecurity
 public class SecurityConfig extends JwtSecurityConfigurerAdapter {
 
-	private final StudentService studentService;
-
 	private final StudentJwtProperties jwtProperties;
 
-	public SecurityConfig(StudentService studentService, StudentJwtProperties jwtProperties) {
-		this.studentService = studentService;
+	public SecurityConfig(StudentJwtProperties jwtProperties) {
 		this.jwtProperties = jwtProperties;
-	}
-
-	@Override
-	public MemberAuthService<Student> memberServiceUsingSecurity() {
-		return this.studentService;
 	}
 
 	@Override
--- src/main/java/com/shouwn/oj/controller/user/StudentController.java
+++ src/main/java/com/shouwn/oj/controller/user/StudentController.java
@@ -1,51 +1,43 @@
 package com.shouwn.oj.controller.user;
 
-import com.shouwn.oj.exception.InvalidParameterException;
 import com.shouwn.oj.model.entity.member.Student;
-import com.shouwn.oj.model.request.member.MemberLoginRequest;
-import com.shouwn.oj.model.response.ApiDataBuilder;
+import com.shouwn.oj.model.request.user.StudentInformation;
 import com.shouwn.oj.model.response.ApiResponse;
 import com.shouwn.oj.model.response.CommonResponse;
-import com.shouwn.oj.security.JwtProvider;
+import com.shouwn.oj.security.CurrentUser;
 import com.shouwn.oj.service.user.StudentServiceForMobile;
-import org.apache.commons.lang3.StringUtils;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("student")
+@RequestMapping("students")
 public class StudentController {
 
-	private final JwtProvider jwtProvider;
+	private final StudentServiceForMobile studentServiceForApi;
 
-	private final StudentServiceForMobile studentServiceForMobile;
-
-	public StudentController(JwtProvider jwtProvider, StudentServiceForMobile studentServiceForMobile) {
-		this.jwtProvider = jwtProvider;
-		this.studentServiceForMobile = studentServiceForMobile;
+	public StudentController(StudentServiceForMobile studentServiceForApi) {
+		this.studentServiceForApi = studentServiceForApi;
 	}
 
-	@PostMapping("login")
-	public ApiResponse<?> login(@RequestBody MemberLoginRequest loginRequest) {
-		Student student;
+	@GetMapping("own")
+	@PreAuthorize("isAuthenticated()")
+	public ApiResponse<?> getSelfInformation(@CurrentUser Long requestId) {
+		Student student = studentServiceForApi.findById(requestId);
 
-		if (StringUtils.isBlank(loginRequest.getUsername()) || StringUtils.isBlank(loginRequest.getPassword())) {
-			throw new InvalidParameterException("아이디 혹은 비밀번호를 입력해주세요.");
-		}
-
-		student = studentServiceForMobile.login(loginRequest.getUsername(), loginRequest.getPassword());
-
-		String jwt = jwtProvider.generateJwt(student.getId());
+		StudentInformation studentInformation = StudentInformation.builder()
+				.name(student.getName())
+				.username(student.getUsername())
+				.email(student.getEmail())
+				.build();
 
 		return CommonResponse.builder()
-				.status(HttpStatus.CREATED)
-				.message("로그인 성공")
-				.data(new ApiDataBuilder().addData("token", jwt).packaging())
+				.status(HttpStatus.OK)
+				.message("개인정보 조회 성공")
+				.data(studentInformation)
 				.build();
 	}
-
 }
