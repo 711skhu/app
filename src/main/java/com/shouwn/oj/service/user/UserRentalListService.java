3b8ab2de23224e7b7796f99cdc2a1100fb29c936
Kim Ji Eun <ys6445@naver.com>
2019-07-21 16:59:45 +0900

    피드백 수정
    페이지네이션 오류 코드 삭제

--- src/main/java/com/shouwn/oj/service/user/UserRentalListService.java
+++ src/main/java/com/shouwn/oj/service/user/UserRentalListService.java
@@ -21,38 +21,15 @@ public class UserRentalListService {
 	public List<UserLectureRentalInfo> rentalList(HtmlPage rentalPage) {
 
 		if (!UrlType.RENTALPAGE_URL.getUrl().equals(rentalPage.getUrl())) {
-
 			throw new IllegalStateException("잘못된 접근 입니다.");
 
 		}
 
 		List<UserLectureRentalInfo> rentalList = new ArrayList<>();
-
 		HtmlTable rentalListTable = (HtmlTable) rentalPage.getElementById("gv대여내역");
-
-		int pageCount = 1;
-
-		try {
 
 			for (int i = 1; i < rentalListTable.getRowCount(); i++) {
 
-				if (rentalListTable.getRow(i).getChildElementCount() == 1) {
-
-					for (HtmlAnchor anchor : rentalPage.getAnchors()) {
-
-						if (anchor.asText().equals(Integer.toString(pageCount + 1))) {
-							rentalPage = anchor.click();
-							Thread.sleep(3000);
-							++pageCount;
-							i = 1;
-							rentalListTable = (HtmlTable) rentalPage.getElementById("gv대여내역");
-							break;
-						}
-					}
-					if (i != 1) {
-						break;
-					}
-				}
 				String rentalState = rentalListTable.getCellAt(i, 0).asText();
 				String lectureCode = rentalListTable.getCellAt(i, 1).asText();
 				int endIndex = lectureCode.indexOf(")");
@@ -64,17 +41,9 @@ public class UserRentalListService {
 				rentalList.add(new UserLectureRentalInfo(i, rentalDate, rentalState, lectureCode, rentalState.equals("승인") ? false : true));
 
 			}
-
 			return rentalList;
 
-		} catch (IOException e) {
-			return ExceptionUtils.rethrow(e);
-		} catch (InterruptedException e) {
-			return ExceptionUtils.rethrow(e);
-		}
-
 	}
-
 
 	public HtmlPage rentalCancel(HtmlPage rentalPage, int idx) {
 
