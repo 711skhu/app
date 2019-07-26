c61ede8ff5b3933c74a77ebdf72a348fb96b1aff
songz1 <21pori30@gmail.com>
2019-07-27 00:10:57 +0900

    UserRentalList pagination 구현 완료

--- src/main/java/com/shouwn/oj/service/user/UserRentalListService.java
+++ src/main/java/com/shouwn/oj/service/user/UserRentalListService.java
@@ -1,19 +1,17 @@
 package com.shouwn.oj.service.user;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
 import com.shouwn.oj.exception.IllegalStateException;
 import com.shouwn.oj.model.enums.user.UrlType;
 import com.shouwn.oj.model.response.rental.RentalDate;
 import com.shouwn.oj.model.response.user.UserLectureRentalInfo;
-
-import java.io.IOException;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
@@ -23,23 +21,20 @@ import org.springframework.stereotype.Service;
 public class UserRentalListService {
 
 	public List<UserLectureRentalInfo> rentalList(HtmlPage rentalPage) {
-
-		if (!UrlType.RENTALPAGE_URL.getUrl().equals(rentalPage.getUrl())) {
-			throw new IllegalStateException("잘못된 접근 입니다.");
-
-		}
-
-		List<UserLectureRentalInfo> rentalList = new ArrayList<>();
-		HtmlTable rentalListTable = (HtmlTable) rentalPage.getElementById("gv대여내역");
-
 		try {
+			if (!UrlType.RENTALPAGE_URL.getUrl().equals(rentalPage.getUrl())) {
+				throw new IllegalStateException("잘못된 접근 입니다.");
+
+			}
+
+			List<UserLectureRentalInfo> rentalList = new ArrayList<>();
+			HtmlTable rentalListTable = (HtmlTable) rentalPage.getElementById("gv대여내역");
+
 			int pageCount = 1;
 			int index = 0;
 			for (int i = 1; i < rentalListTable.getRowCount(); i++) {
 				if (rentalListTable.getRow(i).getChildElementCount() == 1) {
-					if (pageCount == 1) {
-//						System.out.println(rentalPage.getAnchorByHref("javascript:__doPostBack('gv대여내역','Page$" + (pageCount + 1) + "')").isValid());
-//						System.out.println(rentalPage.getAnchorByHref("javascript:__doPostBack('gv대여내역','Page$" + (pageCount + 2) + "')").isValid());
+					if (rentalListTable.getRow(i).getTextContent().contains(Integer.toString(pageCount + 1))) {
 						HtmlAnchor pagination = rentalPage.getAnchorByHref("javascript:__doPostBack('gv대여내역','Page$" + (++pageCount) + "')");
 						rentalPage = pagination.click();
 						Thread.sleep(3000);
@@ -60,7 +55,6 @@ public class UserRentalListService {
 				RentalDate rentalDate = new RentalDate(Integer.parseInt(rowRentalDate.substring(12, 14)), Integer.parseInt(rowRentalDate.substring(19, 21)) + 1, LocalDate.parse(rowRentalDate.substring(0, 10)));
 
 				rentalList.add(new UserLectureRentalInfo(++index, rentalDate, rentalState, lectureCode, rentalState.equals("승인") ? false : true));
-
 			}
 
 			return rentalList;
@@ -72,15 +66,19 @@ public class UserRentalListService {
 	}
 
 	public HtmlPage rentalCancel(HtmlPage rentalPage, int idx) {
-
 		try {
 			for (HtmlAnchor anchor : rentalPage.getAnchors()) {
-				if (StringUtils.equals(("gv대여내역_ctl0" + (idx + 1) + "_btnDelete"), anchor.getId())) {
-					rentalPage = anchor.click();
+				if (StringUtils.equals(("javascript:__doPostBack('gv대여내역','Page$" + (idx / 10 + 1) + "')"), anchor.getHrefAttribute())) {
+					HtmlAnchor pagination = rentalPage.getAnchorByHref("javascript:__doPostBack('gv대여내역','Page$" + (idx / 10 + 1) + "')");
+					rentalPage = pagination.click();
 					Thread.sleep(3000);
-					break;
 				}
 			}
+
+			HtmlAnchor cancel = rentalPage.getAnchorByHref("javascript:__doPostBack('gv대여내역$ctl0" + (idx % 10 + 1) + "$btnDelete','')");
+			cancel.click();
+			Thread.sleep(3000);
+
 			return rentalPage;
 		} catch (InterruptedException e) {
 			return ExceptionUtils.rethrow(e);
